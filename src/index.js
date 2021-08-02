bf1f3499a8c7464958bf2df0bc0a3b57db65889d
priya <priyachowdhury06583@gmail.com>
2021-08-02 13:25:50 +0530

    greeting_app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+const name="Priya";
+let currTime=new Date();
+currTime=currTime.getHours();
+let greeting='';
+const cssStyle={};
+if(currTime>=1 && currTime<12)
+{
+  greeting="Good Morning";
+  cssStyle.color="green";
+}
+else if(currTime>=12 && currTime<19)
+{
+  greeting="Good Afternoon";
+  cssStyle.color="orange";
+}
+else
+{
+  greeting="Good Night";
+  cssStyle.color="black";
+  
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div>
+    <h1>Hello {name},<span style={cssStyle}>{greeting}</span></h1>
+    </div> ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
